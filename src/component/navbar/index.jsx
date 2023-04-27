81db28df73c84da3de21b94b7d75ed164c5218f8
ankit <ankit@themetromaxgroup.com>
2023-04-27 09:03:23 +0530

    navbar

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./navbar.css"
 
 const Navbar = () => {
+  const [pathname,setPathName]=useState("home")
   return (
     <nav className='nav_bar'>
         {/* <div className='icon'><img alt='icon' src=""/></div> */}
         <div className='icon_name'>bigrox</div>
-   <ul>
-    <li ><Link to='/'>Home</Link></li>
-    <li><Link to='/'>Pricing</Link></li>
-    <li><Link to='/'>Knowledge Hub</Link></li>
-    <li className='active'><Link to='/'>Let's Connect</Link></li>
+   <ul onClick={(e)=>setPathName(e.target.id)}>
+    <li className={`${pathname==="home"&&"active"}`}><Link to='/' id='home'>Home</Link></li>
+    <li className={`${pathname==="pricing"&&"active"}`}><Link to='/' id='pricing'>Pricing</Link></li>
+    <li className={`${pathname==="hub"&&"active"}`}><Link to='/' id='hub'>Knowledge Hub</Link></li>
+    <li className={`${pathname==="connect"&&"active"}`}><Link to='/' id='connect'>Let's Connect</Link></li>
    </ul>
     </nav>
   )
